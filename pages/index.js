475cf87b68b13b5c06de8649269b8e8d5153f872
kartikeysharmaks <kartikeysharma1616@gmail.com>
2022-08-24 18:41:24 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -26,12 +26,12 @@ export async function getServerSideProps(context) {
 
   const request = await fetch(
     `https://api.themoviedb.org/3${
-      requests[genre]?.url || requests.fetchTrending.url
+      requests?.url || requests.fetchTrending.url
     }/`
   ).then((res) => res.json());
   return {
     props: {
-      results: request,
+      results: request.results,
     },
   };
 };
